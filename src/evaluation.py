14f9594272982a362afb16a753c009da9fbb80a1
Stoyan Dimitrov <stoyan.dimitrov@dtms.de>
2020-04-20 20:14:44 +0200

    complete f1 score

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,21 +1,46 @@
 import jsonlines
+from collections import OrderedDict
 
 
-def build_confusion_matrix(predicted, labeled):
+def build_confusion_matrix(labeled, predicted):
     """
     Matrix of overlaps and mismatches
     :param predicted: list: labels the model predict for each sample
     :param labeled: list:  actual labels for sample
-    :return: label_size x label_size matrix of int
+    :return: dict: number of overlaps between original and predicted labels, for each label
     """
+    # initialize for {orig1: {pred1:number, ..}, orig2: {pred:number, ..}, ...}
+    label_types = set(labeled)
+    as_keys = sorted(list(label_types))
+    conf_matrix = OrderedDict.fromkeys(as_keys)
+    for l in conf_matrix.keys():
+        conf_matrix[l] = OrderedDict.fromkeys(as_keys, 0)
 
+    # go through the labels and increment for the matches:
+    for orig, pred in zip(labeled, predicted):
+        conf_matrix[orig][pred] += 1
 
-def get_mertrics(confusion_matrix):
+    return conf_matrix
+
+def get_metrics(confusion_matrix):
     """
     get FP, FN, TP, TN: needed for precision and recall
-    :param confusion_matrix
-    :return: int: TP, int: TN, int: FP , int: FN
+    :param confusion_matrix: as dict
+    :return: dict: TP, dict: TN, dict: FP
     """
+    tp, tn, fp ,fn = {}, {}, {}, {}
+    #by_label = dict.fromkeys(['tp', 'fp', 'tn', 'fn'], {})
+
+    # check label by label the types of confusion
+    all_labels = confusion_matrix.keys()
+    for label, row in confusion_matrix.items():
+        tp[label] = row[label]
+        # original label confused with predicted labels
+        fn[label] = sum(row.values()) - tp[label]
+        # predicted label confusing original labels:
+        fp[label] = sum([confusion_matrix[l][label] for l in all_labels]) - tp[label]
+
+    return tp, fn, fp
 
 
 def f1_score(predicted, labeled):
@@ -26,7 +51,12 @@ def f1_score(predicted, labeled):
     :return: int: F1 score
     """
     confusion_matrix = build_confusion_matrix(predicted, labeled)
-    tp, tn, fp, fn = get_mertrics(confusion_matrix)
+    tp, fn, fp = get_metrics(confusion_matrix)
+
+    # sum up all metric results, collapsing the labels:
+    tp = sum(tp.values())
+    fn = sum(fn.values())
+    fp = sum(fp.values())
 
     try:
         precision = tp / (tp + fp)
@@ -43,3 +73,8 @@ def dev_labels(filename):
         for obj in reader:
             label_list.append(obj['label'])
     return label_list
+
+
+label_list = dev_labels('data/scicite/dev.jsonl')
+pred_list = ['method'] * len(label_list)
+f1 = f1_score(label_list, pred_list)
