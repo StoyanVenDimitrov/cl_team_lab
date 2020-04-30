887e7df2a0129c512395a4ed02b6dbd05d917238
e-tony <20404466+e-tony@users.noreply.github.com>
2020-04-30 15:35:42 +0200

    Update docs

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -107,7 +107,6 @@ class BOW:
         """
         Function to generate bag-of-words representation of text.
         :param text: string, this should be the entire text in a document
-        :param threshold: int, number to choose the top words sorted word frequency list
         :param min_occurrence: int, minimum number of occurrences for words
         :param sort: bool, parameter for sorting word frequency
         :return: list: list of lists containing integers
